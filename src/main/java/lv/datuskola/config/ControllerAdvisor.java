9f0690e9e504244c074ff332c11fc010e1608d18
kaspars.rinkevics <1180349+rinkevics@users.noreply.github.com>
2020-03-21 18:54:52 +0200

    log exceptions

--- src/main/java/lv/datuskola/config/ControllerAdvisor.java
+++ src/main/java/lv/datuskola/config/ControllerAdvisor.java
@@ -1,15 +1,21 @@
 package lv.datuskola.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(Exception.class)
     public String handleCityNotFoundException(Exception ex, WebRequest request) {
+        String url = ((ServletWebRequest) request).getRequest().getRequestURL().toString();
+        logger.error("Log exception in " + url, ex);
         return "error";
     }
 
