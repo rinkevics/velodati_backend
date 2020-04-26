9c0ec2af50e35fe8239f2821565b38f98fe9f407
Kaspars Rinkevics <1180349+rinkevics@users.noreply.github.com>
2020-04-26 20:25:59 +0300

    test startup script

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 read -s mySecret
 export mySecret
 
-nohup java -jar -Xms128m -Xmx1024m "target/velodati-1.0-SNAPSHOT.jar" secrets-file="/home/velo/velodati_backend/secrets.properties" password="$(printenv mySecret)"
+(printenv mySecret | nohup java -jar -Xms128m -Xmx1024m "target/velodati-1.0-SNAPSHOT.jar" secrets-file="/home/velo/velodati_backend/secrets.properties") & disown
